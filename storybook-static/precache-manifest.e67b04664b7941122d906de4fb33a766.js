69ef4f14f1a9e44a113bacd42b33f75749b55c6a
Richard Huf <>
2020-07-10 18:10:31 +1000

    initial static site build

---
+++ storybook-static/precache-manifest.e67b04664b7941122d906de4fb33a766.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af298012d317e74a819defbcfde0cf47",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5c8013daf719e432d0d7.bundle.js"
+  },
+  {
+    "url": "runtime~main.5c8013daf719e432d0d7.bundle.js"
+  },
+  {
+    "url": "vendors~main.5c8013daf719e432d0d7.bundle.js"
+  },
+  {
+    "url": "vendors~main.5c8013daf719e432d0d7.bundle.js.LICENSE.txt"
+  }
+]);
