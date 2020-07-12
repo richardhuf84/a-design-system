d5aed825ce628f547646eea6272144a998d183da
Richard Huf <>
2020-07-12 17:36:41 +1000

    Tertiary button using modifiers class

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,18 +5,18 @@ export default class Button extends React.Component {
 
   render() {
     const Button = styled.button`
-      font-size: ${({ theme: { typography: { size: { body } } } }) => body
-      }rem;
+      
+      &.button {
+        font-size: ${({ theme: { typography: { size: { body } } } }) => body}rem;
       border: 0;
       text-align: center;
       font-weight: ${({ theme: { typography: { weight: { bold } } } }) => bold};
       padding: ${props => props.theme.spacing.s3}rem ${props => props.theme.spacing.s4}rem;
       color: ${ props => props.theme.color.light.default};
       border-radius: ${ props => props.theme.border.radius};
-      background-color: ${ props => this.props.primary ? props.theme.color.primary.default : props.theme.color.secondary.default};
 
       &:hover {
-        background-color: ${ props => this.props.primary ? props.theme.color.primary.dark : props.theme.color.secondary.dark};
+          background-color: ${ props => props.theme.color.grey.default};
         transition: background-color 200ms ease;
         text-decoration: underline;
       }
@@ -28,19 +28,62 @@ export default class Button extends React.Component {
       }
 
       &:disabled {
-        background-color: ${props => this.props.primary ? props.theme.color.primary.light : props.theme.color.secondary.light};
+          background-color: ${props => props.theme.color.grey.default};
         cursor: not-allowed;
+      }
+
+        /* Modifiers */
+
+        &--primary {
+          background-color: ${ props => props.theme.color.primary.default};
+
+          &:hover {
+            background-color: ${props => props.theme.color.primary.dark};
+          }
+
+          &:disabled {
+            background-color: ${props => props.theme.color.primary.light};
+          }
+        }
+
+        &--secondary {
+          background-color: ${ props => props.theme.color.secondary.default};
+
+          &:hover {
+            background-color: ${props => props.theme.color.secondary.dark};
+          }
+
+          &:disabled {
+            background-color: ${props => props.theme.color.secondary.light};
+          }
+        }
+
+        &--tertiary {
+          background-color: ${props => props.theme.color.dark.default};
+        
+          &:hover {
+            background-color: ${props => props.theme.color.dark.dark};
+          }
+
+          &:disabled {
+            background-color: ${props => props.theme.color.dark.light};
+          }
+        }
       }
       `;
 
-
     const style = this.props.style;
-    const type = this.props.primary ? 'primary' : 'secondary';
     const label = this.props.label;
     const disabled = this.props.disabled;
+    const { primary, secondary, tertiary } = this.props;
+    const modifierClass = primary && 'primary' || secondary && `secondary` || tertiary && 'tertiary';
 
     return <Theme>
-      <Button style={{ ...style }} className={`button button--${type}`} disabled={disabled}>
+      <Button
+        style={{ ...style }}
+        className={`button button--${modifierClass}`}
+        disabled={disabled}
+      >
         {label}
       </Button>
     </Theme>
