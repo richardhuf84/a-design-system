468c622fce53f7a85d82ed4bbe73dc11ca6cc7b2
Richard Huf <>
2020-07-14 23:34:33 +1000

    Moving styled component out of render function, and drying out styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,73 +1,61 @@
 import React from "react";
 import styled, { css } from 'styled-components';
 import Theme from '../../Theme';
-export default class Button extends React.Component {
 
-  render() {
-    const style = this.props.style;
-    const label = this.props.label;
-    const disabled = this.props.disabled;
-    const { primary, secondary, tertiary } = this.props;
-    const modifierClass = (primary && 'primary') || (secondary && `secondary`) || (tertiary && 'dark');
+const StyledButton = styled.button.attrs(props => ({
+  disabled: props.disabled
+}))`
+    font-size: ${({ theme: { typography: { size: { body } } } }) => body}rem;
+    font-family: 'Montserrat', sans-serif;
+    border: 0;
+    text-align: center;
+    font-weight: ${({ theme: { typography: { weight: { bold } } } }) => bold};
+    padding: ${props => props.theme.spacing.s3}rem ${props => props.theme.spacing.s4}rem;
+    color: ${ props => props.theme.color.light.default};
+    border-radius: ${ props => props.theme.border.radius};
+    appearance: unset;
+    
+    &:hover {
+      background-color: ${ props => props.theme.color.grey.default};
+      transition: background-color 200ms ease;
+      text-decoration: underline;
+    }
 
-    const modifiers = (modifier) => css`
-      &--${modifier} {
-        background-color: ${ props => props.theme.color[modifier].default};
+    &:focus {
+      outline: 2px dotted ${props => props.theme.color.light.default};
+      outline-offset: -2px;
+      text-decoration: underline;
+    }
 
-        &:hover {
-          background-color: ${props => props.theme.color[modifier].dark};
-        }
+    &:disabled {
+      background-color: ${props => props.theme.color.grey.default};
+      cursor: not-allowed;
+    }
 
-        &:disabled {
-          background-color: ${props => props.theme.color[modifier].light};
-        }
-      }
-    `;
 
-    const Button = styled.button`
-      
-      &.button {
-        font-size: ${({ theme: { typography: { size: { body } } } }) => body}rem;
-        font-family: 'Montserrat', sans-serif;
-        border: 0;
-        text-align: center;
-        font-weight: ${({ theme: { typography: { weight: { bold } } } }) => bold};
-        padding: ${props => props.theme.spacing.s3}rem ${props => props.theme.spacing.s4}rem;
-        color: ${ props => props.theme.color.light.default};
-        border-radius: ${ props => props.theme.border.radius};
+    // TODO turn into a function
+    ${props => props.primary &&
+    css`
+      background-color: ${props.theme.color.primary.default}
+      `};
 
-        &:hover {
-          background-color: ${ props => props.theme.color.grey.default};
-          transition: background-color 200ms ease;
-          text-decoration: underline;
-        }
+      ${props => props.secondary &&
+    css`
+      background-color: ${props.theme.color.secondary.default}
+      `};
 
-        &:focus {
-          outline: 2px dotted ${props => props.theme.color.light.default};
-          outline-offset: -2px;
-          text-decoration: underline;
-        }
+      ${props => props.tertiary &&
+    css`
+      background-color: ${props.theme.color.dark.default}
+      `};
+`;
 
-        &:disabled {
-          background-color: ${props => props.theme.color.grey.default};
-          cursor: not-allowed;
-        }
 
-        /* Modifiers */
-        ${modifiers(modifierClass)}
-      }
-    `;
-
-    return <Theme>
-      <Button
-        style={{ ...style }}
-        className={`button button--${modifierClass}`}
-        disabled={disabled}
-      >
-        {label}
-      </Button>
-    </Theme>
-  }
+export default (props) => {
+  return (<Theme>
+    <StyledButton {...props}>{props.label}</StyledButton>
+  </Theme>
+  )
 };
 
 
