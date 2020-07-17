fff4dc277bb0e3245dd1e7b31493c5bca527ee58
Richard Huf <richardhuf84@gmail.com>
2020-07-17 17:40:03 +1000

    Use createStory helper for adding design props to button stories

--- src/components/Button/1-Button.stories.js
+++ src/components/Button/1-Button.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withDesign } from 'storybook-addon-designs'
 import { action } from '@storybook/addon-actions';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
+import createStory from '../../helpers/createStory.helper';
 import Button from './Button';
 
 export default {
@@ -12,40 +13,13 @@ export default {
 
 export const Primary = () => <Button disabled={boolean("Disabled", false)} onClick={action('clicked')} primary label="Primary Button" />;
 
-Primary.story = {
-  name: 'Button Primary',
-  parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=129%3A0',
-      allowFullscreen: true,
-    },
-  },
-}
 
 export const Secondary = () => <Button disabled={boolean("Disabled", false)} onClick={action('clicked')} secondary label="Secondary Button" />;
 
-Secondary.story = {
-  name: 'Button Secondary',
-  parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=129%3A17',
-      allowFullscreen: true,
-    },
-  },
-}
-
 export const Tertiary = () => <Button disabled={boolean("Disabled", false)} onClick={action('clicked')} tertiary label="Tertiary Button" />;
 
-Tertiary.story = {
-  name: 'Button Tertiary',
-  parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=191%3A0',
-      allowFullscreen: true,
-    },
-  },
-}
+createStory(Primary, 'Button Primary', 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=129%3A0')
 
+createStory(Secondary, 'Button Secondary', 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=129%3A17')
+
+createStory(Tertiary, 'Button Tertiary', 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=191%3A0')
