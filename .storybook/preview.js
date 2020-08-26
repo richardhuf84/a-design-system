3639a95fcf25ceacf4aac7b0b6b381f86367a2b6
Richard Huf <richardhuf84@gmail.com>
2020-08-26 22:40:33 +1000

    fixing broken typography and jsxDecorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { GlobalStyle } from '../src/shared/global';
-const { jsxDecorator } = require("storybook-addon-jsx");
+import { jsxDecorator } from 'storybook-addon-jsx';
 import '@storybook/addon-console';
 
 addDecorator(story => (
--- src/components/Text/Text.stories.js
+++ src/components/Text/Text.stories.js
@@ -15,21 +15,20 @@ const tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'small'];
 const defaultTag = 'p';
 const weights = ['regular', 'bold', 'black'];
 
-export const typography = tags.map((tag) => {
-  return <div>
-    <Text tag="small" size="small" color="grey" style={{ display: 'block' }}>{tag}</Text>
-    <Text
-      tag={tag}
-      size={tag}
-      weight="regular"
-      color="dark"
-      key={tag}
-    >
-      {textContent}
-    </Text >
-  </div >
-
-});
+// export const typography = tags.map((tag) => {
+//   return <div>
+//     <Text tag="small" size="small" color="grey" style={{ display: 'block' }}>{tag}</Text>
+//     <Text
+//       tag={tag}
+//       size={tag}
+//       weight="regular"
+//       color="dark"
+//       key={tag}
+//     >
+//       {textContent}
+//     </Text >
+//   </div >
+// });
 
 export const paragraph = () =>
   <Text tag={select('Tag', tags, defaultTag)} size={select('Size', tags, defaultTag)} weight={select('Weight', weights, 'regular')} color="dark">{textContent}</Text >;
@@ -38,4 +37,4 @@ export const small = () => <Text tag="small" size="small" weight={select('Weight
 
 createStory(paragraph, 'Paragraph', 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=231%3A54');
 
-createStory(typography, 'Typography', 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=231%3A54');
+// createStory(typography, 'Typography', 'https://www.figma.com/file/ZiPw8PslQzYjpWPkPIRwGu/A-Design-System?node-id=231%3A54');
